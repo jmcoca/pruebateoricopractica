e8e69bdbdaacb897a9aad2b9b3cf5a4b21dfb0c1
JoseMCocadelaTorre <jmcoca@gmail.com>
2020-04-26 18:47:47 +0200

    Se han añadido los dos aapellidos

--- hola.php
+++ hola.php
@@ -3,6 +3,9 @@
   <title>Prueba de PHP</title>
  </head>
  <body>
- <?php echo '<p>Hola, soy Jose M Coca de la Torre</p>'; ?>
+ 	<?php
+	echo '<p>Hola, soy Jose M Coca de la Torre</p>';
+	echo '<p>Como ya he puesto los apellidos añado este parrafo<p>';
+	 ?>
  </body>
 </html>
